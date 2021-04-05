80d58e238374f40f3f5d5229a8fe0cf9baec7aa0
BaslaHassine <turkibaslahassine@gmail.com>
2021-04-05 20:34:57 +0100

    first commit

--- redfield/src/app/app.module.ts
+++ redfield/src/app/app.module.ts
@@ -1,29 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-
-
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { DemandCardComponent } from './components/demand-card/demand-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-
+    AuthenticationComponent,
+    DemandCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
